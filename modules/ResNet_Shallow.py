2095bf70b470162f397c17776642fdded4ec2d3a
Euphoria Yan <ysq58000@foxmail.com>
2020-12-07 23:20:25 +0800

    update

---
+++ modules/ResNet_Shallow.py
@@ -0,0 +1,93 @@
+
+import os
+import sys
+
+from torch import nn
+
+class ResNet(nn.Module):
+
+    def __init__(self, input_channel, output_channel, block, layers, page_orient='horizontal'):
+        super(ResNet, self).__init__()
+        # self.blur = blur
+        self.page_orient = page_orient
+
+        self.output_channel_block = [int(output_channel / 4), int(output_channel / 2), output_channel, output_channel]
+
+        self.inplanes = int(output_channel / 8)
+        self.conv0_1 = nn.Conv2d(input_channel, int(output_channel / 16),
+                                 kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn0_1 = nn.BatchNorm2d(int(output_channel / 16))
+        self.conv0_2 = nn.Conv2d(int(output_channel / 16), self.inplanes,
+                                 kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn0_2 = nn.BatchNorm2d(self.inplanes)
+        self.relu = nn.ReLU(inplace=True)
+
+        self.maxpool1 = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
+        self.layer1 = self._make_layer(block, self.output_channel_block[0], layers[0])
+        self.conv1 = nn.Conv2d(self.output_channel_block[0], self.output_channel_block[0],
+                               kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(self.output_channel_block[0])
+
+        self.maxpool2 = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
+        self.layer2 = self._make_layer(block, self.output_channel_block[1], layers[1], stride=1)
+        self.conv2 = nn.Conv2d(self.output_channel_block[1], self.output_channel_block[1],
+                               kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(self.output_channel_block[1])
+
+        if self.page_orient == 'horizontal':
+            self.maxpool3 = nn.MaxPool2d(kernel_size=2, stride=(2, 1), padding=(0, 1))
+        elif self.page_orient == 'vertical':
+            self.maxpool3 = nn.MaxPool2d(kernel_size=2, stride=(1, 2), padding=(1, 0))
+
+        self.layer3 = self._make_layer(block, self.output_channel_block[2], layers[2], stride=1)
+        self.conv3 = nn.Conv2d(self.output_channel_block[2], self.output_channel_block[2],
+                               kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(self.output_channel_block[2])
+
+        self.layer4 = self._make_layer(block, self.output_channel_block[3], layers[3], stride=1)
+
+        if self.page_orient == 'horizontal':
+            self.conv4_1 = nn.Conv2d(self.output_channel_block[3], self.output_channel_block[3],
+                                     kernel_size=2, stride=(2, 1), padding=(0, 1), bias=False)
+        elif self.page_orient == 'vertical':
+            self.conv4_1 = nn.Conv2d(self.output_channel_block[3], self.output_channel_block[3],
+                                     kernel_size=2, stride=(1, 2), padding=(1, 0), bias=False)
+
+        self.bn4_1 = nn.BatchNorm2d(self.output_channel_block[3])
+        self.conv4_2 = nn.Conv2d(self.output_channel_block[3], self.output_channel_block[3],
+                                 kernel_size=2, stride=1, padding=0, bias=False)
+        self.bn4_2 = nn.BatchNorm2d(self.output_channel_block[3])
+
+    def _make_layer(self, block, planes, blocks, stride=1):
+        downsample = None
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = nn.Sequential(
+                nn.Conv2d(self.inplanes, planes * block.expansion,
+                          kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(planes * block.expansion),
+            )
+
+        layers = []
+        layers.append(block(self.inplanes, planes, stride, downsample))
+        self.inplanes = planes * block.expansion
+        for i in range(1, blocks):
+            layers.append(block(self.inplanes, planes))
+
+        return nn.Sequential(*layers)
+
+    # input - (batch_size, channel, H, W)
+    def forward(self, x):
+        x = self.conv0_1(x)  # (batch_size, 32, H, W)
+        x = self.bn0_1(x)
+        x = self.relu(x)
+        x = self.conv0_2(x)  # (batch_size, 64, H, W)
+        x = self.bn0_2(x)
+        x1 = self.relu(x)
+
+        x = self.maxpool1(x1)  # (batch_size, 64, H/2, W/2)
+        x = self.layer1(x)  # (batch_size, 128, H/2, W/2)
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x2 = self.relu(x)
+
+        return x1, x2
--- segment_img.py
+++ segment_img.py
@@ -1,12 +1,211 @@
 import os
 import sys
+import time
 
 import cv2
 import numpy as np
 import argparse
+import collections
+
+import torch
+from torch import nn
+import torch.utils.data
+
+from dataset import AlignCollate, RawDataset, LmdbDataset
+from modules.ResNet_Shallow import ResNet
 
 
-def profile_method(img):
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None):
+        super(BasicBlock, self).__init__()
+        self.conv1 = self._conv3x3(inplanes, planes)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = self._conv3x3(planes, planes)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.relu = nn.ReLU(inplace=True)
+        self.downsample = downsample
+        self.stride = stride
+
+    def _conv3x3(self, in_planes, out_planes, stride=1):
+        "3x3 convolution with padding"
+        return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
+                         padding=1, bias=False)
+
+    def forward(self, x):
+        residual = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        if self.downsample is not None:
+            residual = self.downsample(x)
+        out += residual
+        out = self.relu(out)
+
+        return out
+
+
+class ResNet_segment_text(nn.Module):
+    def __init__(self, ckpt_path, free_CNN=True):
+        super(ResNet_segment_text, self).__init__()
+
+        self.CNN = ResNet(1, 512, BasicBlock, [1, 2, 5, 3], 'vertical')
+
+        self.CNN.requires_grad_(False)
+
+        self.load_CNN_weight(ckpt_path)
+        self.AdaptiveAvgPool = nn.AdaptiveAvgPool2d((None, 1))  # Transform final (imgH/16-1) -> 1
+        self.Prediction = nn.Linear(512, 2)
+        self.Loss = nn.BCELoss()
+
+    def load_CNN_weight(self, ckpt_path):
+        restore_ckpt = collections.OrderedDict()
+        ckpt = torch.load(ckpt_path)
+        for k, v in ckpt.items():
+            if k.startswith('module.FeatureExtraction.ConvNet'):
+                k = k.split('.')
+                new_k = '.'.join(k[3:])
+                restore_ckpt[new_k] = v
+        self.CNN.load_state_dict(restore_ckpt)
+
+    def forward(self, img, logits=None):
+        visual_feature1, visual_feature2 = self.CNN(img)
+        visual_feature = visual_feature1
+
+        visual_feature = self.AdaptiveAvgPool(visual_feature.permute(0, 2, 1, 3))
+        visual_feature = visual_feature.squeeze(3)
+
+        prediciton = self.predict(visual_feature.contiguous())
+        if logits is None:
+            return prediciton.detach()
+        else:
+            loss = self.Loss(prediciton, logits)
+            return loss
+
+
+def nn_method_vertical_train():
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model = ResNet_segment_text('saved_models/Line_baseline_xl/best_norm_ED.pth')
+    model.to(device)
+    # prepare data. two demo images from https://github.com/bgshih/crnn#run-demo
+    AlignCollate_demo = AlignCollate(imgH=100, imgW=32, keep_ratio_with_pad=False)
+    Opt = collections.namedtuple('Opt', ['data_filtering_off'])
+    opt = Opt(True)
+
+    train_data = LmdbDataset(root='datasets/split', opt=opt)
+    train_loader = torch.utils.data.DataLoader(train_data, batch_size=128, shuffle=False, num_workers=4,
+                                              collate_fn=AlignCollate_demo, pin_memory=True)
+    val_data = LmdbDataset(root='datasets/split', opt=opt)
+    val_loader = torch.utils.data.DataLoader(val_data, batch_size=64, shuffle=False, num_workers=4,
+                                             collate_fn=AlignCollate_demo, pin_memory=True)
+
+    # filter that only require gradient decent
+    filtered_parameters = []
+    params_num = []
+    for p in filter(lambda p: p.requires_grad, model.parameters()):
+        filtered_parameters.append(p)
+        params_num.append(np.prod(p.size()))
+    print('Trainable params num : ', sum(params_num))
+
+    optimizer = torch.optim.Adam(filtered_parameters, lr=3e-4)
+    print("Optimizer:", optimizer)
+
+    start_time = time.time()
+    best_accuracy = -1
+    best_norm_ED = -1
+    iteration = 0
+
+    feature_length = 100
+
+    while True:
+        # train part
+        for image_tensors, labels in train_loader:
+            batch_size = image_tensors.size(0)
+            image = image_tensors.to(device)
+            labels = [','.split(label) for label in labels]
+            logits = torch.zeros((batch_size, feature_length))
+            for i in range(batch_size):
+                for label in labels:
+                    label = int(label)
+                    logits[label / feature_length] = 1
+            logits = logits.to(device)
+            model.zero_grad()
+            loss = model(image, logits)
+            loss.backward()
+            optimizer.step()
+
+            iteration += 1
+
+            if (iteration + 1) == 100000:
+                break
+
+        # validation part
+        if (iteration + 1) % 20000 == 0 or iteration == 0:  # To see training progress, we also conduct validation when 'iteration == 0'
+
+            # for log
+            with open(f'./saved_models/{opt.exp_name}/log_train.txt', 'a', encoding='utf-8') as log:
+                model.eval()
+                with torch.no_grad():
+                    valid_loss, current_accuracy, current_norm_ED, preds, confidence_score, labels, infer_time, length_of_data = validation(
+                        model, criterion, valid_loader, converter, opt)
+                model.train()
+
+                # training loss and validation loss
+                loss_log = f'[{iteration + 1}/{opt.num_iter}] Train loss: {loss_avg.val():0.5f}, Valid loss: {valid_loss:0.5f}, Elapsed_time: {elapsed_time:0.5f}'
+                loss_avg.reset()
+
+                current_model_log = f'{"Current_accuracy":17s}: {current_accuracy:0.4f}, {"Current_norm_ED":17s}: {current_norm_ED:0.4f}'
+
+                # keep best accuracy model (on valid dataset)
+                if current_accuracy > best_accuracy:
+                    best_accuracy = current_accuracy
+                    torch.save(model.state_dict(), f'./saved_models/{opt.exp_name}/best_accuracy.pth')
+                if current_norm_ED > best_norm_ED:
+                    best_norm_ED = current_norm_ED
+                    torch.save(model.state_dict(), f'./saved_models/{opt.exp_name}/best_norm_ED.pth')
+                best_model_log = f'{"Best_accuracy":17s}: {best_accuracy:0.4f}, {"Best_norm_ED":17s}: {best_norm_ED:0.4f}'
+
+                loss_model_log = f'{loss_log}\n{current_model_log}\n{best_model_log}'
+                print(loss_model_log)
+                log.write(loss_model_log + '\n')
+
+                # show some predicted results
+                dashed_line = '-' * 80
+                head = f'{"Ground Truth":25s} | {"Prediction":25s} | Confidence Score & T/F'
+                predicted_result_log = f'{dashed_line}\n{head}\n{dashed_line}\n'
+                for gt, pred, confidence in zip(labels[:5], preds[:5], confidence_score[:5]):
+                    if 'Attn' in opt.Prediction:
+                        gt = gt[:gt.find('[s]')]
+                        pred = pred[:pred.find('[s]')]
+
+                    predicted_result_log += f'{gt:25s} | {pred:25s} | {confidence:0.4f}\t{str(pred == gt)}\n'
+                predicted_result_log += f'{dashed_line}'
+                print(predicted_result_log)
+                log.write(predicted_result_log + '\n')
+
+        # save model per 1e+5 iter.
+        if (iteration + 1) % 20000 == 0:
+            os.makedirs('./saved_models/split/', exist_ok=True)
+            torch.save(model.state_dict(), f'./saved_models/split/iter_{iteration + 1}.pth')
+
+        if (iteration + 1) == 100000:
+            torch.save(model.state_dict(), f'./saved_models/split/iter_{iteration + 1}.pth')
+            print('end the training')
+            sys.exit()
+
+
+def nn_method_vertical(img):
+    net = ResNet_segment_text('saved_models/Line_baseline_xl/best_norm_ED.pth')
+    net.load_state_dict(torch.load('saved_models/Segment/best.pth'))
+
+
+def cv_method_horizontal(img):
     '''
     Segmentation of line image to single characters. Based on horizontal profile.
     '''
@@ -21,28 +220,6 @@ def profile_method(img):
 
     m = np.max(profile)
     print("Profile shape:", profile.shape)
-    #     vis = np.zeros((100, profile.shape[0]))
-    #     print (vis.shape)
-    #
-    #     print ("Max:", m)
-    #
-    #     for i in range(profile.shape[0]):
-    #         #print (profile[i])
-    #         h = int(float(profile[i]) / m * 100)
-    #         #print (h)
-    #         for j in range(100-h,100):
-    #             vis[j,i] = 255
-
-    #     cv2.imshow('image', vis)
-    #     cv2.waitKey(0)
-    #     cv2.destroyAllWindows()
-    # cv2.imwrite("profile.png", vis)
-
-    # imgComposed = np.zeros((inverted.shape[0] + 100 + 2, inverted.shape[1]))
-    # imgComposed[0:100,:] = vis
-    # imgComposed[100 + 2 :,:] = img
-
-    # cv2.imwrite("profile_visu.png", imgComposed)
 
     candidates = []
     threshold = 2
@@ -71,7 +248,7 @@ def profile_method(img):
     return candidates
 
 
-def profile_method_2(img):
+def cv_method_vertical(img):
     '''
     Segmentation of line image to single characters. Based on horizontal profile.
     '''
@@ -81,31 +258,8 @@ def profile_method_2(img):
     result = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
     m = np.max(profile)
 
-    #     vis = np.zeros((100, profile.shape[0]))
-    #     print (vis.shape)
-    #
-    #     print ("Max:", m)
-    #
-    #     for i in range(profile.shape[0]):
-    #         #print (profile[i])
-    #         h = int(float(profile[i]) / m * 100)
-    #         #print (h)
-    #         for j in range(100-h,100):
-    #             vis[j,i] = 255
-
-    #     cv2.imshow('image', vis)
-    #     cv2.waitKey(0)
-    #     cv2.destroyAllWindows()
-    # cv2.imwrite("profile.png", vis)
-
-    # imgComposed = np.zeros((inverted.shape[0] + 100 + 2, inverted.shape[1]))
-    # imgComposed[0:100,:] = vis
-    # imgComposed[100 + 2 :,:] = img
-
-    # cv2.imwrite("profile_visu.png", imgComposed)
-
     candidates = []
-    threshold = 5
+    threshold = 10
     state = 0  # 0 = gap
     lastGap = -1
     for i in range(profile.shape[0]):
@@ -131,5 +285,6 @@ def profile_method_2(img):
 
 
 if __name__ == '__main__':
-    img = cv2.imread('test_line_image/true_line/20201024234435.png', 0)
-    profile_method_2(img)
+    img = cv2.imread('test_line_image/true_line/20201024234320.png', 0)
+    # cv_method_vertical(img)
+    nn_method_vertical(img)
