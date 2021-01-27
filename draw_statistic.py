6ef689194b56c88ec165a7e80f34644213e8c4c2
Euphoria Yan <ysq58000@foxmail.com>
2021-01-27 21:06:26 +0800

    update

---
+++ draw_statistic.py
@@ -0,0 +1,29 @@
+# -*- encoding: utf-8 -*-
+__author__ = 'Euphoria'
+import json
+import matplotlib.pyplot as plt
+
+
+if __name__ == '__main__':
+    with open('result/extract.json', 'r', encoding='utf-8') as fp:
+        total_length_map = json.load(fp)
+
+    total_length_map = {int(k): v for k, v in total_length_map.items()}
+
+    max_length = max(total_length_map.keys())
+
+    print('max length: {}'.format(max_length))
+
+    x = [_ for _ in range(1, max_length + 1)]
+    y = [total_length_map[_] for _ in range(1, max_length + 1)]
+
+    avg = sum([k*v for k, v in total_length_map.items()]) / sum(total_length_map.values())
+    print('avg length: {}'.format(avg))
+
+    plt.figure()
+    plt.bar(x, height=y)
+    plt.grid(True)
+    plt.xlabel('length')
+    plt.ylabel('count')
+    plt.savefig('./statictics.png')
+    plt.show()
