6a6735ae91d4892eb2fd5c6d0cf642f6b6f5c5c8
Euphoria Yan <ysq58000@foxmail.com>
2021-01-27 16:37:03 +0800

    update

--- get_statistic.py
+++ get_statistic.py
@@ -9,6 +9,7 @@ import matplotlib.pyplot as plt
 from collections import defaultdict
 from pprint import pprint
 from tqdm import tqdm
+import lmdb
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -17,24 +18,33 @@ def parse_args():
     return args
 
 
+def get_lmdb_statistics(root):
+    length_map = defaultdict(int)
+    env = lmdb.open(root, max_readers=32, readonly=True, lock=False, readahead=False, meminit=False)
+    with env.begin(write=False) as txn:
+        nSamples = int(txn.get('num-samples'.encode()))
+        for index in tqdm(range(nSamples)):
+            index += 1  # lmdb starts with 1
+            label_key = 'label-%09d'.encode() % index
+            label = txn.get(label_key).decode('utf-8')
+            length_map[len(label)] += 1
+    return length_map
+
+
 if __name__ == '__main__':
     args = parse_args()
-    length_map = defaultdict(int)
-    for file in tqdm(os.listdir(args.path)):
-        name, ext = os.path.splitext(file)
-        if ext.lower() != '.txt':
-            continue
-        with open(os.path.join(args.path, file), 'r', encoding='utf-8') as fp:
-            s = fp.readline().rstrip()
-            length_map[len(s)] += 1
-    pprint(length_map)
-    keys = length_map.keys()
-    max_length = max(*keys)
+    total_length_map = defaultdict(int)
+    for lmdb_root in os.listdir(args.path):
+        length_map = get_lmdb_statistics(os.path.join(args.path, lmdb_root))
+        for k, v in length_map.items():
+            total_length_map[k] += v
+    pprint(total_length_map)
+    max_length = max(total_length_map.keys())
     x = [_ for _ in range(1, max_length+1)]
-    y = [length_map[_] for _ in range(1, max_length+1)]
+    y = [total_length_map[_] for _ in range(1, max_length+1)]
 
-    plt.figure(figsize=(7, 5))
-    plt.hist(y, bins=25)
+    plt.figure()
+    plt.hist(y)
     plt.grid(True)
     plt.xlabel('length')
     plt.ylabel('count')
