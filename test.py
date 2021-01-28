7f1bbe9067a34c96ba9f12dbf767f3960e44ceec
Euphoria Yan <ysq58000@foxmail.com>
2021-01-28 12:12:21 +0800

    update

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ import time
 import string
 import argparse
 import re
+from collections import defaultdict
 
 import torch
 from torch.backends import cudnn
@@ -191,6 +192,116 @@ def validation(model, criterion, evaluation_loader, converter, opt):
     return valid_loss_avg.val(), accuracy, norm_ED, preds_str, confidence_score_list, labels, infer_time, length_of_data
 
 
+def validation_by_length(model, criterion, evaluation_loader, converter, opt):
+    """ validation or evaluation """
+    n_correct = defaultdict(float)
+    norm_ED = defaultdict(float)
+    length_of_data = defaultdict(int)
+    infer_time = 0
+    valid_loss_avg = Averager()
+
+    for i, (image_tensors, labels) in enumerate(evaluation_loader):
+        batch_size = image_tensors.size(0)
+        # length_of_data = length_of_data + batch_size
+        image = image_tensors.to(device)
+        # For max length prediction
+        length_for_pred = torch.IntTensor([opt.batch_max_length] * batch_size).to(device)
+        text_for_pred = torch.LongTensor(batch_size, opt.batch_max_length + 1).fill_(0).to(device)
+
+        text_for_loss, length_for_loss = converter.encode(labels, batch_max_length=opt.batch_max_length)
+
+        start_time = time.time()
+        if 'CTC' in opt.Prediction:
+            preds = model(image, text_for_pred)
+            forward_time = time.time() - start_time
+
+            # Calculate evaluation loss for CTC deocder.
+            preds_size = torch.IntTensor([preds.size(1)] * batch_size)
+            # permute 'preds' to use CTCloss format
+            cost = criterion(preds.log_softmax(2).permute(1, 0, 2), text_for_loss, preds_size, length_for_loss)
+
+            # Select max probabilty (greedy decoding) then decode index to character
+            _, preds_index = preds.max(2)
+            preds_str = converter.decode(preds_index.data, preds_size.data)
+
+        else:
+            preds, alphas = model(image, text_for_pred, is_train=False)
+            forward_time = time.time() - start_time
+
+            preds = preds[:, :text_for_loss.shape[1] - 1, :]
+            target = text_for_loss[:, 1:]  # without [GO] Symbol
+            cost = criterion(preds.contiguous().view(-1, preds.shape[-1]), target.contiguous().view(-1))
+
+            # select max probabilty (greedy decoding) then decode index to character
+            _, preds_index = preds.max(2)
+            preds_str = converter.decode(preds_index, length_for_pred)
+            labels = converter.decode(text_for_loss[:, 1:], length_for_loss)
+
+        infer_time += forward_time
+        valid_loss_avg.add(cost)
+
+        # calculate accuracy & confidence score
+        preds_prob = F.softmax(preds, dim=2)
+        preds_max_prob, _ = preds_prob.max(dim=2)
+        confidence_score_list = []
+        for gt, pred, pred_max_prob in zip(labels, preds_str, preds_max_prob):
+            if 'Attn' in opt.Prediction:
+                gt = gt[:gt.find('[s]')]
+                pred_EOS = pred.find('[s]')
+                pred = pred[:pred_EOS]  # prune after "end of sentence" token ([s])
+                pred_max_prob = pred_max_prob[:pred_EOS]
+
+            # To evaluate 'case sensitive model' with alphanumeric and case insensitve setting.
+            if opt.sensitive and opt.data_filtering_off:
+                pred = pred.lower()
+                gt = gt.lower()
+                alphanumeric_case_insensitve = '0123456789abcdefghijklmnopqrstuvwxyz'
+                out_of_alphanumeric_case_insensitve = f'[^{alphanumeric_case_insensitve}]'
+                pred = re.sub(out_of_alphanumeric_case_insensitve, '', pred)
+                gt = re.sub(out_of_alphanumeric_case_insensitve, '', gt)
+
+            length_of_data[len(gt)] += 1
+
+            if pred == gt:
+                n_correct[len(gt)] += 1
+
+            '''
+            (old version) ICDAR2017 DOST Normalized Edit Distance https://rrc.cvc.uab.es/?ch=7&com=tasks
+            "For each word we calculate the normalized edit distance to the length of the ground truth transcription."
+            if len(gt) == 0:
+                norm_ED += 1
+            else:
+                norm_ED += edit_distance(pred, gt) / len(gt)
+            '''
+
+            # ICDAR2019 Normalized Edit Distance
+            if len(gt) == 0 or len(pred) == 0:
+                norm_ED[len(gt)] += 0
+            elif len(gt) > len(pred):
+                norm_ED[len(gt)] += 1 - edit_distance(pred, gt) / len(gt)
+            else:
+                norm_ED[len(gt)] += 1 - edit_distance(pred, gt) / len(pred)
+
+            # calculate confidence score (= multiply of pred_max_prob)
+            try:
+                confidence_score = pred_max_prob.cumprod(dim=0)[-1]
+            except:
+                confidence_score = 0  # for empty pred case, when prune after "end of sentence" token ([s])
+            # log = open(f'./result/{opt.exp_name}/log_all_evaluation.txt', 'a')
+            # log.write(f'pred: {pred}, gt: {gt}, {pred == gt}, prob: {confidence_score:0.4f}\n')
+            # log.close()
+            confidence_score_list.append(confidence_score)
+            # print(pred, gt, pred == gt, confidence_score.item())
+
+    accuracy = defaultdict(float)
+    norm_ED = defaultdict(float)
+    for k in n_correct.keys():
+        accuracy[k] = n_correct[k] / float(length_of_data[k]) * 100
+        norm_ED[k] = norm_ED[k] / float(length_of_data[k]) * 100
+
+    return valid_loss_avg.val(), accuracy, norm_ED, preds_str, confidence_score_list, labels, infer_time, length_of_data
+
+
 def test(opt):
     """ model configuration """
     if 'CTC' in opt.Prediction:
@@ -238,12 +349,22 @@ def test(opt):
                 num_workers=int(opt.workers),
                 collate_fn=AlignCollate_evaluation, pin_memory=True)
             # valid_loss_avg.val(), accuracy, norm_ED, preds_str, confidence_score_list, labels, infer_time, length_of_data
-            _, accuracy, norm_ED, preds_str, confidence_score_list, _, _, length_of_data = validation(
-                model, criterion, evaluation_loader, converter, opt)
-            log.write(eval_data_log)
-            print(f'ACC: {accuracy:0.3f}, norm_ED: {norm_ED:0.3f}')
-            log.write(f'ACC: {accuracy:0.3f}, norm_ED: {norm_ED:0.3f}\n')
-            log.close()
+            if opt.by_length:
+                _, accuracy, norm_ED, preds_str, confidence_score_list, _, _, length_of_data = validation_by_length(
+                    model, criterion, evaluation_loader, converter, opt)
+                log.write(eval_data_log)
+                length_key = sorted(accuracy.keys())
+                for length in length_key:
+                    print(f'Length: {length}, ACC: {accuracy[length]:0.3f}, norm_ED: {norm_ED[length]:0.3f}')
+                    log.write(f'Length: {length}, ACC: {accuracy[length]:0.3f}, norm_ED: {norm_ED[length]:0.3f}\n')
+                log.close()
+            else:
+                _, accuracy, norm_ED, preds_str, confidence_score_list, _, _, length_of_data = validation(
+                    model, criterion, evaluation_loader, converter, opt)
+                log.write(eval_data_log)
+                print(f'ACC: {accuracy:0.3f}, norm_ED: {norm_ED:0.3f}')
+                log.write(f'ACC: {accuracy:0.3f}, norm_ED: {norm_ED:0.3f}\n')
+                log.close()
 
 
 if __name__ == '__main__':
@@ -274,6 +395,8 @@ if __name__ == '__main__':
                         help='the number of output channel of Feature extractor')
     parser.add_argument('--hidden_size', type=int, default=256, help='the size of the LSTM hidden state')
     parser.add_argument('--page_orient', type=str, choices=['horizontal', 'vertical'])
+
+    parser.add_argument('--by_length', action='store_true')
 
     opt = parser.parse_args()
 
