84a9eb07f26393f6d5cb97c6ce468233b26b3644
Euphoria Yan <ysq58000@foxmail.com>
2021-01-28 12:12:21 +0800

    update

--- draw_statistic.py
+++ draw_statistic.py
@@ -3,6 +3,8 @@ __author__ = 'Euphoria'
 import json
 import matplotlib.pyplot as plt
 
+from collections import defaultdict
+
 
 if __name__ == '__main__':
     with open('result/extract.json', 'r', encoding='utf-8') as fp:
@@ -15,7 +17,7 @@ if __name__ == '__main__':
     print('max length: {}'.format(max_length))
 
     x = [_ for _ in range(1, max_length + 1)]
-    y = [total_length_map[_] for _ in range(1, max_length + 1)]
+    y = [total_length_map.get(_, 0) for _ in range(1, max_length + 1)]
 
     avg = sum([k*v for k, v in total_length_map.items()]) / sum(total_length_map.values())
     print('avg length: {}'.format(avg))
